88a8b9b58e39c2c9f558f38c33094f0eb1d30b65
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-05-22 19:30:14 +0530

    Closes #2 normal

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 function Feedback() {
   return (
     <div>Feedback</div>
+    //need to add feedback form and functionality
+    //to send feedback to the server and display success or error message
+    //also need to add validation for the form fields and handle errors
+    
   )
 }
 
